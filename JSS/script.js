dc571ad31e717048d86bfdba3b107fe24267fdc5
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-23 23:48:28 +0200

    crato l'array vip e l'array tavolo vip

---
+++ JSS/script.js
@@ -0,0 +1,58 @@
+                            //! SNACK 1
+//< array con i vip 
+
+const Vip=[ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
+
+const TavoloVip=[
+      {
+    tableName : 'tavolo1',
+    guestName : '2',
+       place:    'occupied' },
+
+       {
+        tableName : 'tavolo2',
+        guestName : '2',
+           place:    'occupied' },
+           {
+            tableName : 'tavolo3',
+            guestName : '2',
+               place:    'occupied' },
+               {
+                tableName : 'tavolo4',
+                guestName : '2',
+                   place:    'free' },
+                   {
+                    tableName : 'tavolo5',
+                    guestName : '2',
+                       place:    'occupied' },
+                       {
+                        tableName : 'tavolo6',
+                        guestName : '2',
+                           place:    'occupied' },
+                           {
+                            tableName : 'tavolo7',
+                            guestName : 'i',
+                               place:    'occupied' },
+                               {
+                                tableName : 'tavolo8',
+                                guestName : '2',
+                                   place:    'free' },
+                                   {
+                                    tableName : 'tavolo9',
+                                    guestName : '',
+                                       place:    'occupied' },
+                                       {
+                                        tableName : 'tavolo10',
+                                        guestName : '2',
+                                           place:    'free' },
+                                           {
+                                            tableName : 'tavolo11',
+                                            guestName : '2',
+                                               place:    'free' }
+
+
+];
+
+
+
+//> inserisco gli invitati nel array
