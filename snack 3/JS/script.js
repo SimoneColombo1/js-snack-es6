bd86ba46796a6ecd532f17042ab69655f97e0727
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-27 03:22:00 +0200

    creato l'array

--- snack 3/JS/script.js
+++ snack 3/JS/script.js
@@ -1,1 +1,12 @@
 
+  //> Array biciclette 
+const biciclette= [
+    { nome: 'Bianchi', peso: 8 },
+    { nome: 'Pinarello', peso: 7 },
+    { nome: 'Colnago', peso: 9 },
+    {nome: 'Fuji', peso: 7}
+  ];
+
+  //?For each per trovare la bicicletta più leggera
+
+  
